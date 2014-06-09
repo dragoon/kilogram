2ecd2e06cbffb416c05b72f3c5f882ddc2bcdf01
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-06-09 19:46:09 +0200

    Update insert_to_mongo.py

--- ngram_utils/mongo/insert_to_mongo.py
+++ ngram_utils/mongo/insert_to_mongo.py
@@ -8,6 +8,7 @@ import codecs
 import argparse
 import pymongo
 
+parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('-h', '--host', dest='host', action='store', default='localhost',
                    help='mongodb host')
 parser.add_argument('-d', '--database', dest='db', action='store', required=True,
