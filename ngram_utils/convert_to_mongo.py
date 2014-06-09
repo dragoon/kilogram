a0b00bdc48f14df21e545e904a73ce80dfc3f0c9
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-06-09 18:44:59 +0200

    Create convert_to_mongo.py

---
+++ ngram_utils/convert_to_mongo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Run as: cat google_ngram_file | ./convert_to_mongo | sort -k1,1 -t $'\t' > output
+Then proceed to mongodb import
+"""
+import sys
+import argparse
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--sub', dest='subst_file', action='store',
+                   help='path to sustitutions file')
+args = parser.parse_args()
+print args
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    ngram, count = line.split('\t')
+    words = ngram.split()
+    for i, word in enumerate(words):
+        if word in SUBST_SET:
+            ngram = words[:]
+            ngram[i] = 'SUB'
+            ngram.append(str(i))
+            print '\t'.join([' '.join(ngram), word, count])
