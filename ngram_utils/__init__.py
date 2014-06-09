52b1b234b9a0763fb16a4730fdd66496a525dfd9
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-06-09 21:30:07 +0200

    Update __init__.py

--- ngram_utils/__init__.py
+++ ngram_utils/__init__.py
@@ -1,9 +1,9 @@
-"""Stack Exchange Data Dumps processing utils"""
+"""N-gram processing utils"""
 
 __author__      = 'Roman Prokofyev'
 __license__     = 'Apache License 2.0'
 __version__     = '0.1'
 __maintainer__  = __author__
 __email__       = 'roman.prokofyev@gmail.com'
-__url__         = 'https://github.com/dragoon/stackexchange-utils/'
+__url__         = 'https://github.com/dragoon/ngram-utils/'
 __summary__     = __doc__
