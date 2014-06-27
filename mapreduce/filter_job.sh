1d013c037c4ed8e98ceb615722ff2158b4e7b27e
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-06-27 16:01:10 +0200

    update script

--- mapreduce/filter_job.sh
+++ mapreduce/filter_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 REDUCERS=${1:-20}
-hadoop fs -rm -r /home/roman/ngrams_filtered
+hadoop fs -rm -r /user/roman/ngrams_filtered
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
   -Dmapreduce.framework.name=yarn \
   -Dmapreduce.job.contract=false \
   -Dmapreduce.job.reduces=$REDUCERS \
-  -files /home/roman/mapper_generic.py,/home/roman/reducer_generic.py \
+  -files ./mapper_generic.py,./reducer_generic.py \
   -mapper mapper_generic.py \
   -reducer reducer_generic.py \
   -input /data/ngrams -output /user/roman/ngrams_filtered
