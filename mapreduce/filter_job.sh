709b8d3fb436e70d69482b2add4eff3c5d9d4269
Roman <roman@diufpc301.unifr.ch>
2014-07-03 14:09:04 +0200

    fixing arguments

--- mapreduce/filter_job.sh
+++ mapreduce/filter_job.sh
@@ -8,7 +8,7 @@ N=0
 REDUCERS=20
 FILTER_FILE=""
 
-while getopts "rnf:" opt; do
+while getopts "r:n:f:" opt; do
     case "$opt" in
     n)  N=$OPTARG
         ;;
@@ -29,9 +29,9 @@ hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
   -Dmapreduce.framework.name=yarn \
   -Dmapreduce.job.contract=false \
   -Dmapreduce.job.reduces=$REDUCERS \
+  -files ./filter/mapper_filter.py,reducer_generic.py,words.txt \
   -cmdenv NGRAM=$N \
-  -files filter/mapper_filter.py,reducer_generic.py,words.txt \
-  -mapper filter/mapper_filter.py \
+  -mapper ./filter/mapper_filter.py \
   -reducer reducer_generic.py \
   -input $1 -output $2
 rm words.txt
