6d0420ff966c21e83f4628cc1b34b97d28686ce9
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-07-03 14:17:09 +0200

    updated generic job with getopts

--- mapreduce/generic_job.sh
+++ mapreduce/generic_job.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-REDUCERS=${3:-20}
+
+# A POSIX variable
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Initialize our own variables:
+REDUCERS=20
+
+while getopts "r:" opt; do
+    case "$opt" in
+    r)  REDUCERS=$OPTARG
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
 hadoop fs -rm -r $2
 cp ../extra/cities15000.zip .
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
