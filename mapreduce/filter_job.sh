e1c46dbe52c0df1018e46c40a8b77608f5563fd9
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-07-03 13:13:37 +0200

    added file options

--- mapreduce/filter_job.sh
+++ mapreduce/filter_job.sh
@@ -6,12 +6,15 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 # Initialize our own variables:
 N=0
 REDUCERS=20
+FILTER_FILE=""
 
-while getopts "rn:" opt; do
+while getopts "rnf:" opt; do
     case "$opt" in
     n)  N=$OPTARG
         ;;
     r)  REDUCERS=$OPTARG
+        ;;
+    f) FILTER_FILE=$OPTARG
         ;;
     esac
 done
@@ -20,12 +23,8 @@ shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
-echo $1
-echo $2
-exit
-
 hadoop fs -rm -r $2
-cp ../extra/preps.txt words.txt
+cp $FILTER_FILE words.txt
 hadoop jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
   -Dmapreduce.framework.name=yarn \
   -Dmapreduce.job.contract=false \
