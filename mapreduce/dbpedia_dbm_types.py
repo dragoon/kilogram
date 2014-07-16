581a69e5c55f9ec629708e99815e79359f715d5b
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-07-16 17:42:37 +0200

    use shelve

--- mapreduce/dbpedia_dbm_types.py
+++ mapreduce/dbpedia_dbm_types.py
@@ -1,16 +1,15 @@
 """
 Prepares DBPedia type dict with entity URIs as keys and types as values.
-We use pickle here since the dict is quite small and we need a set as value.
+We use shelve here since the dict is quite large in memory(~2G) and we need a set as value.
 It then shipped with the job.
 """
-from collections import defaultdict
-import pickle
+import shelve
 import subprocess
 
 TYPES_FILE = 'instance_types_en.nt.bz2'
 EXCLUDES = {'<Agent>', '<TimePeriod>', 'PersonFunction'}
 
-dbpediadb = defaultdict(list)
+dbpediadb = shelve.open('dbpedia_types.dbm')
 # BZ2File module cannot process multi-stream files, so use subprocess
 p = subprocess.Popen('bzcat -q ' + TYPES_FILE, shell=True, stdout=subprocess.PIPE)
 for line in p.stdout:
@@ -24,6 +23,9 @@ for line in p.stdout:
     type_uri = type_uri.replace('http://dbpedia.org/ontology/', '')[:-3]
     if type_uri in EXCLUDES:
         continue
-    dbpediadb[uri].append(type_uri)
+    if not dbpediadb.has_key(uri):
+        dbpediadb[uri] = [type_uri]
+    else:
+        dbpediadb[uri].append(type_uri)
 
-pickle.dump(dbpediadb, open('dbpedia_types.dbm', 'w'))
+dbpediadb.close()
