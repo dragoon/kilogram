92ec9b56028a7e70694c26911dcd9d71bae33e58
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-07-16 18:45:06 +0200

    added dbpedia types mapper

---
+++ mapreduce/filter/mapper_dbpedia_types.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys
+import shelve
+
+# Open just for read
+dbpediadb = shelve.open('dbpedia_types.dbm')
+
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    ngram, num = line.split('\t')
+
+    words = ngram.split()
+    dbpedia_words = [(i, word) for i, word in enumerate(words) if word[0] == '<' and word[-1] == '>']
+    dbp_dict = {}
+    for i, word in dbpedia_words:
+        if dbpediadb.has_key(word):
+            dbp_dict[i] = dbpediadb[word]
+    ngrams = [words[:]]
+    new_ngrams = []
+    for i, types in dbp_dict.items():
+        for words in ngrams:
+            for dbp_type in types:
+                new_words = words[:]
+                new_words[i] = dbp_type
+                new_ngrams.append(new_words)
+        ngrams = new_ngrams
+
+    for new_words in ngrams:
+        print '%s\t%s' % (' '.join(new_words), num)
+
+dbpediadb.close()
