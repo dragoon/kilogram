ed41797de943482422c470b414c65421512818de
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-07-31 15:03:02 +0200

    postfilter update

--- mapreduce/filter/mapper_postfilter.py
+++ mapreduce/filter/mapper_postfilter.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 import sys
+import os
+import nltk
 from kilogram.lang import number_replace
+
+FILTER_FILE = os.environ['FILTER_FILE']
+FILTER = set(open(FILTER_FILE).read().splitlines())
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
@@ -9,6 +14,18 @@ for line in sys.stdin:
     line = line.strip()
     # split the line into words
     orig_ngram, num = line.split('\t')
+
+    words = orig_ngram.split()
+    stop = 0
+    for i in range(len(words), 0, -1):
+        for j, ngram in enumerate(nltk.ngrams(words, i)):
+            if ' '.join(ngram) in FILTER:
+                stop = 1
+                break
+        if stop:
+            break
+    if stop:
+        continue
 
     new_words = []
     for word in orig_ngram.split():
@@ -20,6 +37,7 @@ for line in sys.stdin:
             if new_word != word:
                 new_words.append(new_word)
             else:
-                new_words.append(word.lower())
+                # TODO: to lower or not to lower? That is the question.
+                new_words.append(word)
 
     print '%s\t%s' % (' '.join(new_words), num)
