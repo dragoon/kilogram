a4cbac65270075aa8fa7788f44254b3ce8332be8
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-05 17:43:32 +0200

    lm data export script

---
+++ lm/store_data_lm.py
@@ -0,0 +1,20 @@
+import subprocess
+
+p = subprocess.Popen('hdfs dfs -cat /user/roman/ngrams_merged/*', shell=True, stdout=subprocess.PIPE)
+
+VOCAB_FILE = open('1gms/vocab_cs', 'w')
+N_2_FILE = open('2gms/2gm-0001', 'w')
+N_3_FILE = open('3gms/2gm-0001', 'w')
+
+for line in p.stdout:
+    line_len = len(line.split('\t')[0].split())
+    if line_len == 1:
+        VOCAB_FILE.write(line+'\n')
+    elif line_len == 2:
+        N_2_FILE.write(line+'\n')
+    elif line_len == 3:
+        N_3_FILE.write(line+'\n')
+
+VOCAB_FILE.close()
+N_2_FILE.close()
+N_3_FILE.close()
