7760e588ff971391e9d9771f6ee2367589080ff4
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-06 12:54:48 +0200

    match multiple entities in the same n-gram

--- mapreduce/filter/mapper_dbpedia.py
+++ mapreduce/filter/mapper_dbpedia.py
@@ -8,29 +8,32 @@ import anydbm
 dbpediadb = anydbm.open('dbpedia.dbm', 'r')
 URI_EXCLUDES = set(open('dbpedia_uri_excludes.txt').read().splitlines())
 
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
-    # split the line into words
-    ngram, num = line.split('\t')
 
-    words = ngram.split()
+def resolve_entity(words):
+    """Recursive entity resolution"""
     for i in range(len(words), 0, -1):
-        stop = 0
         for j, ngram in enumerate(nltk.ngrams(words, i)):
             ngram_joined = ' '.join(ngram)
             if ngram_joined in dbpediadb:
                 uri = dbpediadb[ngram_joined]
                 if uri in URI_EXCLUDES:
                     continue
-                stop = True
                 new_words = []
                 new_words.extend(words[:j])
                 new_words.append(uri)
-                new_words.extend(words[j+len(ngram):])
-                new_ngram = ' '.join(new_words)
-                print '%s\t%s' % (new_ngram.strip(), num)
-        if stop:
-            break
+                new_words.extend(resolve_entity(words[j+len(ngram):]))
+                return new_words
+    return words
+
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    orig_ngram, num = line.split('\t')
+    new_words = resolve_entity(orig_ngram.split())
+    new_ngram = ' '.join(new_words)
+
+    if new_ngram != orig_ngram:
+        print '%s\t%s' % (new_ngram.strip(), num)
 
 dbpediadb.close()
