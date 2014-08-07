210137a185c15b4202faa8a95f1718e2fe6e5663
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-07 13:50:33 +0200

    integrate vocabulary preprocess

--- lm/store_data_lm.py
+++ lm/store_data_lm.py
@@ -6,6 +6,7 @@ VOCAB_FILE = open('1gms/vocab_cs', 'w')
 N_2_FILE = open('2gms/2gm-0001', 'w')
 N_3_FILE = open('3gms/3gm-0001', 'w')
 
+print 'Starting HDFS streaming...'
 for line in p.stdout:
     line_len = len(line.split('\t')[0].split())
     if line_len == 1:
@@ -18,3 +19,10 @@ for line in p.stdout:
 VOCAB_FILE.close()
 N_2_FILE.close()
 N_3_FILE.close()
+
+print 'Sorting vocabulary...'
+subprocess.call(["cat 1gms/vocab_cs | sort --parallel=4 -k2,2 -n -r -t $'\t' > vocab_cs1 && mv vocab_cs1 1gms/vocab_cs"], shell=True)
+print 'Removing old vocabulary...'
+subprocess.call(["rm 1gms/vocab_cs.gz"], shell=True)
+print 'Compressing new vocabulary...'
+subprocess.call(["gzip 1gms/vocab_cs"], shell=True)
