3872288ca5fd4f951ecf921812fc44a5576a0894
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-08 12:53:02 +0200

    update dbpedia dbm types

--- mapreduce/dbpedia_dbm_types.py
+++ mapreduce/dbpedia_dbm_types.py
@@ -3,7 +3,7 @@ Creates DBPedia type dict with entity URIs as keys and types as values.
 We use shelve here since the dict is quite large in memory(~2G) and we need a set as value.
 It then shipped with the job.
 
-Format: {'<Tramore>': ['<Town>', '<Settlement>', '<PopulatedPlace>', '<Place>'], ...}
+Format: {'Tramore': ['Town', 'Settlement', 'PopulatedPlace', 'Place'], ...}
 """
 import shelve
 import subprocess
@@ -21,8 +21,8 @@ for line in p.stdout:
         continue
     if 'http://dbpedia.org/ontology/' not in type_uri:
         continue
-    uri = uri.replace('http://dbpedia.org/resource/', '')
-    type_uri = type_uri.replace('http://dbpedia.org/ontology/', '')[:-3]
+    uri = uri.replace('<http://dbpedia.org/resource/', '')[:-1]
+    type_uri = type_uri.replace('<http://dbpedia.org/ontology/', '')[:-4]
     if type_uri in EXCLUDES:
         continue
     if uri not in dbpediadb:
