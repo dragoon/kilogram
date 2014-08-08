0d0c2e1d517906e1ae7b481510cf5e73410eccb4
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-08 15:46:52 +0200

    added separate int regex

--- kilogram/lang/__init__.py
+++ kilogram/lang/__init__.py
@@ -3,6 +3,7 @@ import re
 
 FLOAT_REGEX = r'(?:[1-9]\d*|0)(?:[\.,]\d+)?'
 
+INT_RE = re.compile(r'[1-9]+')
 PERCENT_RE = re.compile(r'\b\d{1,2}([\.,]\d{1,2})?\%(\s|$)')
 NUM_RE = re.compile(FLOAT_REGEX)
 TIME_RE1 = re.compile(r'\b\d{1,2}:\d{2}\b')
@@ -12,7 +13,7 @@ VOL_RE = re.compile(r'\b{0}m3(\s|$)'.format(FLOAT_REGEX))  # often occurs in Goo
 SQ_RE = re.compile(r'\b{0}m2(\s|$)'.format(FLOAT_REGEX))
 
 _RE_NUM_SUBS = [('<AREA>', SQ_RE), ('<VOL>', VOL_RE), ('<PERCENT>', PERCENT_RE),
-                ('<TIME1>', TIME_RE1), ('<TIME2>', TIME_RE2), ('<NUM>', NUM_RE)]
+                ('<TIME1>', TIME_RE1), ('<TIME2>', TIME_RE2), ('<INT>', INT_RE), ('<NUM>', NUM_RE)]
 
 
 def number_replace(word):
