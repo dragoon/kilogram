f1708be9bec0efbbf21f589687468d07167ae124
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-08 10:50:37 +0200

    added directory creation if necessary

--- lm/store_data_lm.py
+++ lm/store_data_lm.py
@@ -15,6 +15,10 @@ MODEL_DIR = args.model_dir
 os.chdir(DATA_DIR)
 p = subprocess.Popen('hdfs dfs -cat /user/roman/ngrams_merged/*', shell=True, stdout=subprocess.PIPE)
 
+for ngram_type_dir in ('1gms', '2gms', '3gms'):
+    if not os.path.exists(ngram_type_dir):
+        os.makedirs(ngram_type_dir)
+
 VOCAB_FILE = open('1gms/vocab_cs', 'w')
 N_2_FILE = open('2gms/2gm-0001', 'w')
 N_3_FILE = open('3gms/3gm-0001', 'w')
@@ -36,7 +40,10 @@ N_3_FILE.close()
 print 'Sorting vocabulary...'
 subprocess.call(["cat 1gms/vocab_cs | sort --parallel=4 -k2,2 -n -r -t $'\t' > vocab_cs1 && mv vocab_cs1 1gms/vocab_cs"], shell=True)
 print 'Removing old vocabulary...'
-subprocess.call(["rm 1gms/vocab_cs.gz"], shell=True)
+try:
+    os.remove("1gms/vocab_cs.gz")
+except OSError:
+    pass
 print 'Compressing new vocabulary...'
 subprocess.call(["gzip 1gms/vocab_cs"], shell=True)
 
