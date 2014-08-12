d951d79d92685a3b76bb6fc3c17e097d74c00b72
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-12 16:44:57 +0200

    added mapper and reducer for orig/lowercase ratio

--- mapreduce/filter/mapper_lowercase.py
+++ mapreduce/filter/mapper_lowercase.py
@@ -4,6 +4,7 @@ import sys
 
 # Open just for read
 dbpediadb = set(open('dbpedia_labels.txt').read().splitlines())
+dbpediadb_lower = set(x.lower() for x in open('dbpedia_labels.txt').read().splitlines())
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -12,5 +13,7 @@ for line in sys.stdin:
     ngram, num = line.split('\t')
 
     if ngram in dbpediadb:
-        print '<%s>\t%s' % (ngram.replace(' ', '_'), num)
+        print '%s\t%s|--|%s' % (ngram.lower(), ngram.replace(' ', '_'), num)
+    if ngram in dbpediadb_lower:
+        print '%s\t%s|--|%s' % (ngram.lower(), 'lower', num)
 
---
+++ mapreduce/filter/reducer_lowercase.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import sys
+
+current_ngram = None
+cur_values = []
+
+
+def process_values(values):
+    orig_ngram = None
+    entity_counts = []
+    # prevent zero division
+    non_entity_counts = [1]
+    for value in values:
+        ngram_type, num = value.replace('|--|')
+        if ngram_type == 'lower':
+            non_entity_counts.append(int(num))
+        else:
+            orig_ngram = ngram_type
+            entity_counts.append(int(num))
+    if orig_ngram:
+        print '%s\t%s' % (orig_ngram, sum(entity_counts)/sum(non_entity_counts))
+
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    ngram, value = line.split('\t')
+
+    if not current_ngram:
+        current_ngram = ngram
+
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if ngram == current_ngram:
+        cur_values.append(value)
+    else:
+        process_values(cur_values)
+        cur_values = []
+        current_ngram = ngram
+
+process_values(cur_values)
