8d73eb6ec2012a5ec0aed2c99b882abab6fc8270
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-22 16:23:31 +0200

    added unicode and punctuation filter

--- mapreduce/wikipedia/wikipedia_text_types.py
+++ mapreduce/wikipedia/wikipedia_text_types.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python
-"""Filters determiners and forms new n-grams with skips"""
-
 import sys
 import shelve
 import anydbm
-import nltk
 
 from kilogram.dataset.wikipedia.entities import parse_types_text
 
+
 dbpedia_redirects = anydbm.open('dbpedia_redirects.dbm', 'r')
 dbpedia_types = shelve.open('dbpedia_types.dbm', flag='r')
-
 
 for line in sys.stdin:
     if not line:
@@ -26,15 +22,10 @@ for line in sys.stdin:
         if i == last and not sentence.endswith('.'):
             continue
         if not sentence.endswith('.'):
-            sentence += ' .'
-        words = sentence.split()
-        for ngram in nltk.ngrams(words, 3):
-            ngram_joined = ' '.join(ngram)
-            if '<dbpedia:' in ngram_joined:
-                print '%s\t%s' % (ngram_joined, 1)
-                for n in (1, 2):
-                    for lower_ngram in nltk.ngrams(ngram, n):
-                        print '%s\t%s' % (' '.join(lower_ngram), 1)
+            print sentence+' .'
+        else:
+            print sentence
+
 
 dbpedia_redirects.close()
 dbpedia_types.close()
