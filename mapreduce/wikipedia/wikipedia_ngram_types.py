8a66fae8b5f2ee329e0ca361022326b4fda9efbd
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-23 21:51:08 +0200

    build all n-grams for wikipedia

--- mapreduce/wikipedia/wikipedia_ngram_types.py
+++ mapreduce/wikipedia/wikipedia_ngram_types.py
@@ -28,13 +28,10 @@ for line in sys.stdin:
         if not sentence.endswith('.'):
             sentence += ' .'
         words = sentence.split()
-        for ngram in nltk.ngrams(words, 3):
-            ngram_joined = ' '.join(ngram)
-            if '<dbpedia:' in ngram_joined:
+        for n in (1,2,3):
+            for ngram in nltk.ngrams(words, n):
+                ngram_joined = ' '.join(ngram)
                 print '%s\t%s' % (ngram_joined, 1)
-                for n in (1, 2):
-                    for lower_ngram in nltk.ngrams(ngram, n):
-                        print '%s\t%s' % (' '.join(lower_ngram), 1)
 
 dbpedia_redirects.close()
 dbpedia_types.close()
