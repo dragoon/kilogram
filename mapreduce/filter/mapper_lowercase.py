70f5fd43aa98d0d49f2ed7703957e506c72e3bc4
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-26 16:07:59 +0200

    optimize mapper lowercase memory usage

--- mapreduce/filter/mapper_lowercase.py
+++ mapreduce/filter/mapper_lowercase.py
@@ -3,8 +3,9 @@
 import sys
 
 # Open just for read
-dbpediadb = set(open('dbpedia_labels.txt').read().splitlines())
-dbpediadb_lower = set(x.lower() for x in open('dbpedia_labels.txt').read().splitlines())
+dbpediadb_lower = dict((x.lower(), set()) for x in open('dbpedia_labels.txt').read().splitlines())
+for label in open('dbpedia_labels.txt'):
+    dbpediadb_lower[label.lower()].add(label)
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -12,7 +13,8 @@ for line in sys.stdin:
     # split the line into words
     ngram, num = line.split('\t')
 
-    if ngram in dbpediadb:
+    label_set = dbpediadb_lower.get(ngram.lower(), set())
+    if ngram in label_set:
         print '%s\t%s|--|%s' % (ngram.lower(), ngram.replace(' ', '_'), num)
     if ngram in dbpediadb_lower:
         print '%s\t%s|--|%s' % (ngram.lower(), 'lower', num)
