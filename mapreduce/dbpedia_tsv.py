ebaeb0d118e9cc43dca85ff06979a96b577f0301
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-27 14:42:30 +0200

    added csv writer

--- mapreduce/dbpedia_tsv.py
+++ mapreduce/dbpedia_tsv.py
@@ -11,6 +11,7 @@ Tramore,_Ireland    Tramore     Town;Settlement;PopulatedPlace;Place
 
 import subprocess
 import urllib
+import unicodecsv as csv
 from collections import defaultdict
 
 TYPES_FILE = 'instance_types_en.nt.bz2'
@@ -33,28 +34,28 @@ for line in p.stdout:
 
     dbpediadb_types[uri].append(type_uri)
 
-dbpedia_types_tsv = open('dbpedia_types.tsv', 'w')
+with open('dbpedia_types.csv', 'w') as out:
+    csvwriter = csv.writer(out, delimiter='\t', encoding='utf-8')
 
-# write types first
-for uri, types in dbpediadb_types.items():
-    dbpedia_types_tsv.write('\t'.join([uri, uri, ';'.join(types)])+'\n')
+    # write types first
+    for uri, types in dbpediadb_types.items():
+        csvwriter.writerow([uri, uri, ';'.join(types)])
 
 
-REDIRECTS_FILE = 'redirects_transitive_en.nt.bz2'
-dbpediadb_labels = {}
-# BZ2File module cannot process multi-stream files, so use subprocess
-p = subprocess.Popen('bzcat -q ' + REDIRECTS_FILE, shell=True, stdout=subprocess.PIPE)
-for line in p.stdout:
-    try:
-        uri_redirect, predicate, uri_canon = line.split(' ', 2)
-    except:
-        continue
-    name_redirect = urllib.unquote(uri_redirect.replace('<http://dbpedia.org/resource/', '')[:-1])
-    name_canon = urllib.unquote(uri_canon.replace('<http://dbpedia.org/resource/', '')[:-4])
-    if '(disambiguation)' in name_redirect:
-        continue
-    # skip entities that have no types
-    if name_canon not in dbpediadb_types:
-        continue
-    dbpedia_types_tsv.write('\t'.join([name_redirect, name_canon, ';'.join(dbpediadb_types[name_canon])])+'\n')
-dbpedia_types_tsv.close()
+    REDIRECTS_FILE = 'redirects_transitive_en.nt.bz2'
+    dbpediadb_labels = {}
+    # BZ2File module cannot process multi-stream files, so use subprocess
+    p = subprocess.Popen('bzcat -q ' + REDIRECTS_FILE, shell=True, stdout=subprocess.PIPE)
+    for line in p.stdout:
+        try:
+            uri_redirect, predicate, uri_canon = line.split(' ', 2)
+        except:
+            continue
+        name_redirect = urllib.unquote(uri_redirect.replace('<http://dbpedia.org/resource/', '')[:-1])
+        name_canon = urllib.unquote(uri_canon.replace('<http://dbpedia.org/resource/', '')[:-4])
+        if '(disambiguation)' in name_redirect:
+            continue
+        # skip entities that have no types
+        if name_canon not in dbpediadb_types:
+            continue
+        csvwriter.writerow([name_redirect, name_canon, ';'.join(dbpediadb_types[name_canon])])
