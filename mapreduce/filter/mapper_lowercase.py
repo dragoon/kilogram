d8061651eca601c2f94da5169842267c41dbf98f
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-08-28 13:25:24 +0200

    memory optimization

--- mapreduce/filter/mapper_lowercase.py
+++ mapreduce/filter/mapper_lowercase.py
@@ -7,12 +7,7 @@ import shelve
 dbpediadb = shelve.open('dbpedia_types.dbm', flag='r')
 dbpediadb_lower = {}
 for key, value in dbpediadb.iteritems():
-    if key.lower() not in dbpediadb_lower:
-        dbpediadb_lower[key.lower()] = {'uri': value['uri'], 'labels': [key]}
-    else:
-        dbpediadb_lower[key.lower()]['labels'].append(key)
-
-dbpediadb.close()
+    dbpediadb_lower[key.lower()] = value['uri']
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -21,9 +16,10 @@ for line in sys.stdin:
     ngram, num = line.split('\t')
     uri_ngram = ngram.replace(' ', '_')
 
+    if uri_ngram in dbpediadb:
+        print '%s\t%s|--|%s' % (dbpediadb[uri_ngram].lower(), dbpediadb[uri_ngram], num)
+
     if uri_ngram in dbpediadb_lower:
-        print '%s\t%s|--|%s' % (dbpediadb_lower[uri_ngram]['uri'].lower(), 'lower', num)
-    elif uri_ngram.lower() in dbpediadb_lower:
-        entity = dbpediadb_lower[uri_ngram.lower()]
-        if uri_ngram in entity['labels']:
-            print '%s\t%s|--|%s' % (entity['uri'].lower(), entity['uri'], num)
+        print '%s\t%s|--|%s' % (dbpediadb_lower[uri_ngram].lower(), 'lower', num)
+
+dbpediadb.close()
