fd1bcd1a1c93158da2fdcd9ef1ee34c9cd7f4137
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-09-26 21:40:48 +0200

    Update mapper_lowercase.py

--- mapreduce/filter/mapper_lowercase.py
+++ mapreduce/filter/mapper_lowercase.py
@@ -6,22 +6,22 @@ import shelve
 
 # Open just for read
 dbpediadb = shelve.open('dbpedia_types.dbm', flag='r')
-dbpediadb_lower = defaultdict(list)
-for key in dbpediadb.iterkeys():
-    dbpediadb_lower[key.lower()].append(key)
+labels = set(dbpediadb.keys())
+dbpediadb.close()
+dbpediadb_lower = shelve.open('dbpedia_lowercase2labels.dbm', flag='r')
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
     ngram, num = line.split('\t')
-    uri_ngram = ngram.replace(' ', '_')
+    label = ngram.replace(' ', '_')
 
-    if uri_ngram in dbpediadb:
-        print '%s\t%s|--|%s' % (uri_ngram, 'orig', num)
+    if label in labels:
+        print '%s\t%s|--|%s' % (label, 'orig', num)
 
-    if uri_ngram in dbpediadb_lower:
-        for key in dbpediadb_lower[uri_ngram]:
+    if label in dbpediadb_lower:
+        for key in dbpediadb_lower[label]:
             print '%s\t%s|--|%s' % (key, 'lower', num)
 
 dbpediadb.close()
