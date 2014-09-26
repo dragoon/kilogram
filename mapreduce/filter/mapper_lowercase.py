414b24e4968ad86af4e8dd64a9d399f207c8b59e
Roman Prokofyev <roman.prokofyev@gmail.com>
2014-09-26 16:52:45 +0200

    updated lowercase

--- mapreduce/filter/mapper_lowercase.py
+++ mapreduce/filter/mapper_lowercase.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
 import sys
 import shelve
 
 # Open just for read
 dbpediadb = shelve.open('dbpedia_types.dbm', flag='r')
-dbpediadb_lower = {}
+dbpediadb_lower = defaultdict(set)
 for key in dbpediadb.iterkeys():
-    dbpediadb_lower[key.lower()] = key
+    dbpediadb_lower[key.lower()].add(key)
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
@@ -20,6 +21,7 @@ for line in sys.stdin:
         print '%s\t%s|--|%s' % (uri_ngram, 'orig', num)
 
     if uri_ngram in dbpediadb_lower:
-        print '%s\t%s|--|%s' % (dbpediadb_lower[uri_ngram], 'lower', num)
+        for key in dbpediadb_lower[uri_ngram]:
+            print '%s\t%s|--|%s' % (key, 'lower', num)
 
 dbpediadb.close()
