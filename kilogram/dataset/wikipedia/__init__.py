bc21b1244df6100c052f841cbe4e1e3517616ce6
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-02-12 16:23:51 +0100

    lowercase sentences

--- kilogram/dataset/wikipedia/__init__.py
+++ kilogram/dataset/wikipedia/__init__.py
@@ -7,8 +7,10 @@ def line_filter(line):
     sentences = END_SENTENCE_RE.split(line)
     last = len(sentences) - 1
     for i, sentence in enumerate(sentences):
+        if not sentence.strip():
+            continue
         if i == last and not sentence.endswith('.'):
             continue
         if not sentence.endswith('.'):
             sentence += ' .'
-        yield sentence
+        yield sentence[0].lower() + sentence[1:]
