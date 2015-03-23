a99276d922131173000f6361c235f73d3fd0ceba
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-03-23 14:58:41 +0100

    new substitution method

--- mapreduce/type_prediction/mapper.py
+++ mapreduce/type_prediction/mapper.py
@@ -2,6 +2,7 @@
 """Mapper to filter and extract types"""
 
 import sys
+from kilogram.ngram_service import SUBSTITUTION_TOKEN
 
 for line in sys.stdin:
     # split the line into words
@@ -11,6 +12,7 @@ for line in sys.stdin:
     if type_count == 1:
         ngram = ngram.split()
         type_index = [i for i, x in enumerate(ngram) if x.startswith('<dbpedia:')][0]
-        entity_type = ngram.pop(type_index)
-        new_ngram = " ".join(ngram)+str(type_index)
+        entity_type = ngram[type_index]
+        ngram[type_index] = SUBSTITUTION_TOKEN
+        new_ngram = " ".join(ngram)
         print '%s\t%s\t%s' % (new_ngram, entity_type, num)
