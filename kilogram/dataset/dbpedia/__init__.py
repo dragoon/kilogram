72b3c2b7b2c703597cf4eba93913bc02d2aa2a76
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-03-26 09:50:09 +0100

    updated dbpedia api

--- kilogram/dataset/dbpedia/__init__.py
+++ kilogram/dataset/dbpedia/__init__.py
@@ -1,19 +1,29 @@
-from collections import defaultdict
 import rdflib
 
 __author__ = 'Roman Prokofyev'
 
 
-def get_dbpedia_type_hierarchy(owl_filename):
-    dbpedia_types = defaultdict(list)
-    g = rdflib.Graph()
-    g.parse(owl_filename, format="xml")
-    for subject, predicate, obj in g:
-        if str(predicate) == 'http://www.w3.org/2000/01/rdf-schema#subClassOf':
-            obj = str(obj)
-            subject = str(subject)
-            if 'http://dbpedia.org/ontology/' in obj and 'http://dbpedia.org/ontology/' in subject:
-                obj = obj.replace('http://dbpedia.org/ontology/', '<dbpedia:') + '>'
-                subject = subject.replace('http://dbpedia.org/ontology/', '<dbpedia:') + '>'
-                dbpedia_types[obj].append(subject)
-    return dbpedia_types
+class DBPediaOntology:
+    dbpedia_types = None
+
+    def __init__(self, owl_filename):
+        self.dbpedia_types = {}
+        g = rdflib.Graph()
+        g.parse(owl_filename, format="xml")
+        for subject, predicate, obj in g:
+            if str(predicate) == 'http://www.w3.org/2000/01/rdf-schema#subClassOf':
+                obj = str(obj)
+                subject = str(subject)
+                if 'http://dbpedia.org/ontology/' in obj \
+                        and 'http://dbpedia.org/ontology/' in subject:
+                    obj = obj.replace('http://dbpedia.org/ontology/', '<dbpedia:') + '>'
+                    subject = subject.replace('http://dbpedia.org/ontology/', '<dbpedia:') + '>'
+                    self.dbpedia_types[subject] = obj
+
+    def get_parent(self, entity_type, dest_parent=None):
+        try:
+            while self.dbpedia_types.get(entity_type, None) != dest_parent:
+                entity_type = self.dbpedia_types[entity_type]
+            return entity_type
+        except KeyError:
+            return None
