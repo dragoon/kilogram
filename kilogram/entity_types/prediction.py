d94c5ad6dc53863d3bf7c1e2e7e38f4e91528904
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-03-26 14:21:02 +0100

    second prediction method

--- kilogram/entity_types/prediction.py
+++ kilogram/entity_types/prediction.py
@@ -1,5 +1,8 @@
 from __future__ import division
 from collections import defaultdict
+
+from scipy.stats import entropy, kurtosis
+
 from .. import NgramService, SUBSTITUTION_TOKEN, ListPacker
 
 
@@ -38,21 +41,58 @@ def predict_types(context, type_hierarchy):
     result_probs = sorted([(entity_type, prob/3) for entity_type, prob in type_probs.iteritems()],
                           key=lambda x: x[1], reverse=True)
     return result_probs
-    result_probs_dict = dict(result_probs)
 
-    correct_types = []
 
-    while type_hierarchy.get(result_probs[0][0], None):
-        parent = result_probs.pop(0)[0]
-        correct_types.append(parent)
-        # get probability from dict, it could be updated
-        parent_prob = result_probs_dict[parent]
-        del result_probs_dict[parent]
+def predict_types_full(context, type_hierarchy):
+    """Context should always be a 5-element list
+    :type type_hierarchy: DBPediaOntology
+    """
+    # pre, post, mid bigrams
+    context[2] = SUBSTITUTION_TOKEN
+    bigrams = [context[:3], context[-3:], context[1:4]]
+    types = []
+    for bigram in bigrams:
+        type_values = NgramService.hbase_raw("ngram_types_test", " ".join(bigram), "ngram:value")
+        if type_values:
+            types.append(ListPacker.unpack(type_values))
+    totals = [sum(int(x) for x in zip(*type_values)[1]) for type_values in types]
+    bigram_probs = [[(entity_type, int(count)/totals[i]) for entity_type, count in type_values] for i, type_values in enumerate(types)]
 
-        existing_children = [child for child in type_hierarchy[parent] if child in result_probs_dict]
-        for child in existing_children:
-            result_probs_dict[child] += parent_prob/len(existing_children)
+    correct_types = [None]
+    while True:
+        parent = correct_types[-1]
+        parent_probs = sum_probabilities(bigram_probs, type_hierarchy, parent)
 
-    residual_types = zip(*sorted(result_probs_dict.items(), key=lambda x: x[1], reverse=True))[0]
-    return correct_types + list(residual_types)
+        if len(parent_probs) == 0:
+            break
+        type_probs = defaultdict(lambda: 0)
+        for probs in parent_probs:
+            for entity_type, prob in probs:
+                type_probs[entity_type] += prob
+        correct_types.append(max(type_probs.items(), key=lambda x: x[1])[0])
+
+        # checks how often the selected type in not on top of n-gram separate ranks
+        if correct_types[-1] not in [max(probs, key=lambda x: x[1])[0] for probs in parent_probs]:
+            print parent_probs
+
+    return list(reversed(correct_types[1:]))
+
+
+def sum_probabilities(probabilities_list, type_hierarchy, parent):
+    new_prob_list = []
+    for probabilities in probabilities_list:
+        prob_dict = defaultdict(lambda: 0)
+        for entity_type, prob in probabilities:
+            entity_type = type_hierarchy.get_parent(entity_type, parent)
+            prob_dict[entity_type] += prob
+        try:
+            none_prob = prob_dict[None]/(len(prob_dict) - 1)
+            del prob_dict[None]
+            for key in prob_dict.keys():
+                prob_dict[key] += none_prob
+
+            new_prob_list.append(prob_dict.items())
+        except:
+            continue
+    return new_prob_list
 
