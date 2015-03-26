6596df6577e4a9a428813f5667e0ad13d761be72
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-03-26 15:03:20 +0100

    app update

--- kilogram/entity_types/app.py
+++ kilogram/entity_types/app.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 from flask import Flask, jsonify, request
-from kilogram.dataset.dbpedia import get_dbpedia_type_hierarchy
-from kilogram.entity_types.prediction import predict_types
+from kilogram.dataset.dbpedia import DBPediaOntology
+from kilogram.entity_types.prediction import TypePredictor
 from kilogram import NgramService
 
 NgramService.configure(hbase_host=('diufpc301', 9090))
 
 app = Flask(__name__)
-dbpedia_type_hierarchy = get_dbpedia_type_hierarchy('dbpedia_2014.owl')
+dbpedia_ontology = DBPediaOntology('dbpedia_2014.owl')
+predictor = TypePredictor("ngram_types", dbpedia_ontology)
 
 @app.route('/', methods=['GET'])
 def predict():
     context = request.args.get('context').strip().split()
-    return jsonify({'types': predict_types(context, dbpedia_type_hierarchy)})
+    return jsonify({'types': predictor.predict_types(context)})
 
 
 if __name__ == '__main__':
