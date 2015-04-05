e2a9fd4939b134233005c20d8eb58b59867eb626
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-04-06 00:38:58 +0200

    added deep learning prediction method to the web service

--- kilogram/entity_types/app.py
+++ kilogram/entity_types/app.py
@@ -9,12 +9,19 @@ NgramService.configure(hbase_host=('diufpc301', 9090))
 
 app = Flask(__name__)
 dbpedia_ontology = DBPediaOntology('dbpedia_2014.owl')
-predictor = TypePredictor("ngram_types", dbpedia_ontology, '/home/roman/notebooks/kilogram/mapreduce/dbpedia_types.dbm')
+predictor = TypePredictor("ngram_types", dbpedia_ontology,
+                          '/home/roman/notebooks/kilogram/mapreduce/dbpedia_types.dbm',
+                          word2vec_model_filename='/home/roman/berkeleylm/300features_40minwords_10context')
 
-@app.route('/', methods=['GET'])
+@app.route('/predict/types/context', methods=['GET'])
 def predict():
     context = strip_unicode(request.args.get('context').strip()).split()
     return jsonify({'types': predictor.predict_types(context)})
+
+@app.route('/predict/types/word', methods=['GET'])
+def predict():
+    ngram = strip_unicode(request.args.get('ngram').strip()).split()
+    return jsonify({'types': predictor._predict_types_from_ngram(ngram)})
 
 
 if __name__ == '__main__':
