b684049fcd38a5937b02dc07d5a07aae815c2222
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-04-10 17:38:49 +0200

    fixed endpoints

--- kilogram/entity_types/app.py
+++ kilogram/entity_types/app.py
@@ -13,21 +13,21 @@ dbpedia_ontology = DBPediaOntology('dbpedia_2014.owl')
 ngram_predictor = NgramTypePredictor("ngram_types", dbpedia_ontology,
                           '/home/roman/notebooks/kilogram/mapreduce/dbpedia_types.dbm')
 deep_predictor = TypePredictionModel('/home/roman/berkeleylm/300features_40minwords_10context',
-                                     '/home/roman/notebooks/kilogram/kilogram/ntity_types/tests/type_prediction_train.tsv')
+                                     '/home/roman/notebooks/kilogram/kilogram/entity_types/tests/type_prediction_train.tsv')
 
 
 @app.route('/predict/types/context', methods=['GET'])
-def predict_context():
+def predict_ngram_from_context():
     context = strip_unicode(request.args.get('context').strip()).split()
     return jsonify({'types': ngram_predictor.predict_types(context)})
 
 @app.route('/predict/types/word/linear', methods=['GET'])
-def predict_nram():
+def deep_predict_linear():
     ngram = strip_unicode(request.args.get('ngram').strip())
     return jsonify({'types': deep_predictor.predict_types_linear(ngram)})
 
 @app.route('/predict/types/word/raw', methods=['GET'])
-def predict_nram():
+def deep_predict_raw():
     ngram = strip_unicode(request.args.get('ngram').strip())
     return jsonify({'types': deep_predictor.predict_types_similarity(ngram)})
 
