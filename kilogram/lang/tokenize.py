1929c5b480018f20644b11e2bf2dce0a19e7b71f
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-08-13 13:34:17 +0200

    new possible splits

--- kilogram/lang/tokenize.py
+++ kilogram/lang/tokenize.py
@@ -60,9 +60,9 @@ def default_tokenize_func(sentence, punct_set=_SIMPLE_PUNCT):
 def generate_possible_splits(words, max_seq_len=None):
     if max_seq_len is None:
         max_seq_len = len(words)
-    start = max(1, min(max_seq_len, len(words) - max_seq_len))
-    for i in range(start, len(words)):
-        yield [words[:i], words[i:]]
+    for i in range(1, min(max_seq_len, len(words))):
+        if len(words) - i >= 5:
+            yield [words[:i], words[i:]]
         if len(words[i:]) > 1:
             for perm in generate_possible_splits(words[i:], max_seq_len):
                 yield [words[:i]] + perm
