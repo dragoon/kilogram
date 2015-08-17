72a282dfd29802eb2ae947813b71633921505d9d
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-08-17 10:16:58 +0200

    fix wikipedia probability

--- kilogram/ngram_service.py
+++ kilogram/ngram_service.py
@@ -138,5 +138,8 @@ class NgramService(object):
         """Get wiki probability of a phrase"""
         page_counts = ListPacker.unpack(NgramService.hbase_raw(cls.wiki_anchors_table, phrase, "ngram:value"))
         anchor_counts = sum([long(x[1]) for x in page_counts])
-        wiki_counts = sum([cls.hbase_count(cls.wiki_urls_table, x[0]) for x in page_counts]) + 1
-        return anchor_counts/wiki_counts
+        wiki_counts = sum([cls.hbase_count(cls.wiki_urls_table, x[0].decode('utf-8')) for x in page_counts])
+        try:
+            return anchor_counts/wiki_counts
+        except ZeroDivisionError:
+            return 0
