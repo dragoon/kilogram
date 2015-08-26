5b61cd7b6b2e6ab841f36fd1d8ab2834a40ea859
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-08-26 16:12:32 +0200

    minor

--- mapreduce/filter/mapper_dbpedia.py
+++ mapreduce/filter/mapper_dbpedia.py
@@ -6,9 +6,7 @@ from kilogram.dataset.dbpedia import NgramEntityResolver
 ner = NgramEntityResolver("dbpedia_types.txt", "dbpedia_uri_excludes.txt", "dbpediadb_lower.txt")
 
 for line in sys.stdin:
-    if not line.strip():
-        print line
-        continue
+    line = line.strip()
     # split the line into words
     new_words = ner.resolve_entities(line.split())
     new_line = ' '.join(new_words)
