cb2dd0a1b3d98902fc8bbfe74430361f3c7a7210
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-08-26 16:07:50 +0200

    mapper

--- mapreduce/filter/mapper_dbpedia.py
+++ mapreduce/filter/mapper_dbpedia.py
@@ -6,8 +6,6 @@ from kilogram.dataset.dbpedia import NgramEntityResolver
 ner = NgramEntityResolver("dbpedia_types.txt", "dbpedia_uri_excludes.txt", "dbpediadb_lower.txt")
 
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
     # split the line into words
     new_words = ner.resolve_entities(line.split())
     new_line = ' '.join(new_words)
