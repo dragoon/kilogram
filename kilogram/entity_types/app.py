d79739a4d596afbc46dc73a7ccd8beaae217ef37
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-08-31 14:45:23 +0200

    config update

--- kilogram/entity_types/app.py
+++ kilogram/entity_types/app.py
@@ -5,10 +5,10 @@ from kilogram.entity_types.prediction import NgramTypePredictor
 from kilogram import NgramService
 from kilogram.lang.unicode import strip_unicode
 
-NgramService.configure(hbase_host=('diufpc301', 9090))
+NgramService.configure(hbase_host=('diufpc304', 9090))
 
 app = Flask(__name__)
-dbpedia_ontology = DBPediaOntology('dbpedia_2014.owl')
+dbpedia_ontology = DBPediaOntology('dbpedia_2015-04.owl')
 ngram_predictor = NgramTypePredictor("ngram_types", dbpedia_ontology,
                           '/home/roman/notebooks/kilogram/mapreduce/dbpedia_types.dbm')
 
