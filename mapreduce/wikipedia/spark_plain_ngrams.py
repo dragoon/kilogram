853f9319edf6fc9800d343a61ef84b06e442467c
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-02 13:20:12 +0200

    generate proper n-grams

--- mapreduce/wikipedia/spark_plain_ngrams.py
+++ mapreduce/wikipedia/spark_plain_ngrams.py
@@ -5,6 +5,8 @@ import sys
 from pyspark import SparkContext
 import re
 import nltk
+from kilogram.lang.tokenize import wiki_tokenize_func
+from kilogram.dataset.wikipedia import line_filter
 
 ENTITY_MATCH_RE = re.compile(r'<(.+?)\|(.+?)>')
 
@@ -18,12 +20,13 @@ def generate_ngrams(line):
     def partition(alist, indices):
         return [alist[i+1:j] for i, j in zip([-1]+indices, indices+[None])]
     result = []
-    line = line.strip().split()
-    entity_indexes = [i for i, word in enumerate(line) if ENTITY_MATCH_RE.search(word)]
-    for sublist in partition(line, entity_indexes):
-        for i in range(1, 6):
-            for ngram in nltk.ngrams(sublist, i):
-                result.append((' '.join(ngram), 1))
+    line = line.strip()
+    for sentence in line_filter(' '.join(wiki_tokenize_func(line))):
+        entity_indexes = [i for i, word in enumerate(sentence) if ENTITY_MATCH_RE.search(word)]
+        for sublist in partition(line, entity_indexes):
+            for i in range(1, 6):
+                for ngram in nltk.ngrams(sublist, i):
+                    result.append((' '.join(ngram), 1))
     return result
 
 ngrams = lines.flatMap(generate_ngrams).reduceByKey(lambda n1, n2: n1 + n2).filter(lambda x, y: y > 1)
