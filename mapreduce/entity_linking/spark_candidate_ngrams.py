5c911f2b0fe2c1d19ca7e85282524f5cca9c8b7a
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-02 16:39:55 +0200

    candidate improvement

--- mapreduce/entity_linking/spark_candidate_ngrams.py
+++ mapreduce/entity_linking/spark_candidate_ngrams.py
@@ -1,5 +1,5 @@
 """
-spark-submit --master yarn-client ./entity_linking/candidate_ngrams_spark.py "/data/wikipedia2015_plaintext_annotated" "/user/roman/wikipedia_anchors_orig" "/user/roman/candidate_ngram_links"
+spark-submit --master yarn-client ./entity_linking/candidate_ngrams_spark.py "/user/roman/wikipedia_anchors_orig" "/user/roman/candidate_ngram_links"
 """
 from collections import defaultdict
 import sys
@@ -13,23 +13,6 @@ ENTITY_MATCH_RE = re.compile(r'<(.+?)\|(.+?)>')
 
 sc = SparkContext(appName="CandidateEntityLinkings")
 
-lines = sc.textFile(sys.argv[1])
-
-# Split each line into words
-def generate_ngrams(line):
-    def partition(alist, indices):
-        return [alist[i+1:j] for i, j in zip([-1]+indices, indices+[None])]
-    result = []
-    line = line.strip().split()
-    entity_indexes = [i for i, word in enumerate(line) if ENTITY_MATCH_RE.search(word)]
-    for sublist in partition(line, entity_indexes):
-        for i in range(1, 2):
-            for ngram in nltk.ngrams(sublist, i):
-                result.append((' '.join(ngram), 1))
-    return result
-
-
-ngrams = lines.flatMap(generate_ngrams).reduceByKey(lambda n1, n2: n1 + n2).filter(lambda x: x[1] > 1)
 
 def generate_anchor_ngrams(line):
     result = []
@@ -49,9 +32,10 @@ def reduce_anchors(v1, v2):
     return ListPacker.pack(res.items())
 
 
-anchors = sc.textFile(sys.argv[2]).flatMap(generate_anchor_ngrams).reduceByKey(reduce_anchors)
-anchors_join = anchors.join(ngrams)
-def printer(value):
-    return value[0] + '\t' + value[1][0] + ' NULL,'+str(value[1][1])
+anchors = sc.textFile(sys.argv[1]).flatMap(generate_anchor_ngrams).reduceByKey(reduce_anchors)
 
-anchors_join.map(printer).saveAsTextFile(sys.argv[3])
+
+def printer(value):
+    return value[0] + '\t' + value[1]
+
+anchors.map(printer).saveAsTextFile(sys.argv[2])
