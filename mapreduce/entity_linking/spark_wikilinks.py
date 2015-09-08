5cc99c037c44a211761146f18627af5a85299ec6
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-08 14:23:46 +0200

    better pagelinks

--- mapreduce/entity_linking/spark_wikilinks.py
+++ mapreduce/entity_linking/spark_wikilinks.py
@@ -1,23 +1,41 @@
 """
-spark-submit --master yarn-client ./entity_linking/spark_reduce_wikilinks.py "/user/roman/wikipedia_pagelinks"
+spark-submit --master yarn-client ./entity_linking/spark_wikilinks.py "/user/roman/wikipedia_pagelinks"
 """
 import sys
 from pyspark import SparkContext
 
 sc = SparkContext(appName="WikipediaPageLinks")
 
+# Redirects to exclude redirect pages
+dbp_redirects_file = sc.textFile("/user/roman/redirects_transitive_en.nt.bz2")
+def map_redirects(line):
+    try:
+        uri, _, canon_uri, _ = line.split()
+    except:
+        return None
+    uri = uri.replace('<http://dbpedia.org/resource/', '')[:-1]
+    if '/' in uri:
+        return None
+    return uri
+
+dbp_redirects = set(dbp_redirects_file.map(map_redirects).collect())
+
 
 dbp_pagelinks_file = sc.textFile("/user/roman/page-links_en.nt.bz2")
 def map_pagelinks(line):
+    if '(disambiguation)' in line:
+        return [None]
     try:
         uri, _, link, _ = line.split()
     except:
         return [None]
     uri = uri.replace('<http://dbpedia.org/resource/', '')[:-1]
+    link = link.replace('<http://dbpedia.org/resource/', '')[:-1]
     if '/' in uri:
         return [None]
-    link = link.replace('<http://dbpedia.org/resource/', '')[:-1]
     if ':' in link:
+        return [None]
+    if uri in dbp_redirects:
         return [None]
     return [(uri, link), (link, uri)]
 
