3fbccb9ef5a11e4162ab5acad047fa4e9c18cfaf
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-11 15:11:08 +0200

    minor

--- mapreduce/entity_linking/hbase_edge_weights.py
+++ mapreduce/entity_linking/hbase_edge_weights.py
@@ -8,15 +8,12 @@ scanner = NgramService.h_client.scannerOpen("wiki_pagelinks", "", ["ngram"], Non
 out = codecs.open('edges.txt', 'w', 'utf-8')
 i = 0
 while True:
-    try:
-        row = NgramService.h_client.scannerGet(scanner)[0]
-        uri = row.row
-        related_uris = set(zip(*ListPacker.unpack(row.columns['ngram:value'].value))[0])
-        for related_uri in related_uris:
-            count = len(set(zip(*NgramService.get_related_uris(related_uri))[0]).intersection(related_uris))
-            out.write(uri+","+related_uri+'\t'+str(count))
-    except:
-        break
+    row = NgramService.h_client.scannerGet(scanner)[0]
+    uri = row.row
+    related_uris = set(zip(*ListPacker.unpack(row.columns['ngram:value'].value))[0])
+    for related_uri in related_uris:
+        count = len(set(zip(*NgramService.get_related_uris(related_uri))[0]).intersection(related_uris))
+        out.write(uri+","+related_uri+'\t'+str(count))
     if not i % 10000:
         print i
     i += 1
