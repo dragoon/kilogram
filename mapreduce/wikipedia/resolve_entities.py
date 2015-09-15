a394e2d31807c9d849658607eea7b5eadc4d6782
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-15 16:34:14 +0200

    minor

--- mapreduce/wikipedia/resolve_entities.py
+++ mapreduce/wikipedia/resolve_entities.py
@@ -6,7 +6,7 @@ cat wekex_test_plain | python ../notebooks/kilogram/mapreduce/wikipedia/resolve_
 import sys
 from kilogram.dataset.dbpedia import NgramEntityResolver
 
-ner = NgramEntityResolver("dbpedia_types.txt", "dbpedia_uri_excludes.txt", "dbpediadb_lower.txt", "dbpedia_ontology.owl")
+ner = NgramEntityResolver("/home/roman/dbpedia/dbpedia_types.txt", "/home/roman/dbpedia/dbpedia_uri_excludes.txt", "/home/roman/dbpedia/dbpedia_lower_includes.txt", "/home/roman/dbpedia/dbpedia_redirects.txt", "/home/roman/dbpedia/dbpedia_2015-04.owl")
 
 for line in sys.stdin:
     line = line.strip()
