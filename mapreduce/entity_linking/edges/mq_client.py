258ead52da30dfb281d45724837d97cac3d83ae1
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-16 14:08:44 +0200

    generate weighted edges

---
+++ mapreduce/entity_linking/edges/mq_client.py
@@ -0,0 +1,36 @@
+import codecs
+import multiprocessing
+import zmq
+
+socket = None
+
+index_map = {}
+index = 0
+
+def initializer():
+    global socket
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("ipc:///tmp/wikipedia_edges")
+
+def uri_map(item):
+    socket.send(item[0]+'|--|'+str(item[1]))
+    return socket.recv()
+
+if __name__ == "__main__":
+    for line in open('labels.txt'):
+        index_map[line.strip()] = index
+        index += 1
+    out = codecs.open('edges.txt', 'w', 'utf-8')
+    pool = multiprocessing.Pool(10, initializer)
+
+    j = 0
+    for res in pool.imap_unordered(uri_map, index_map.iteritems()):
+        if res:
+            out.write(res+'\n')
+        if not j % 10000:
+            print j
+        j += 1
+
+    out.close()
+    pool.close()
---
+++ mapreduce/entity_linking/edges/mq_server.py
@@ -0,0 +1,38 @@
+import zmq
+from kilogram import ListPacker
+
+index_map = {}
+values = []
+index = 0
+
+for line in open('labels.txt'):
+    index_map[line.strip()] = index
+    index += 1
+
+for j, line in enumerate(open('wikipedia_pagelinks.tsv')):
+    label, value = line.strip().split('\t')
+    values.append(zip(*ListPacker.unpack(value))[0])
+    if not j % 10000:
+        print j
+    j += 1
+
+def uri_map(item):
+    res = []
+    uri, i = item.split('|--|')
+    i = int(i)
+    direct_neighbors = set(values[i])
+    for neighbor in direct_neighbors:
+        neighbor_uris = values[index_map[neighbor]]
+        count = len(direct_neighbors.intersection(neighbor_uris))
+        if count > 0:
+            res.append(uri+"|--|"+neighbor+'\t'+str(count))
+    return '\n'.join(res)
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("ipc:///tmp/wikipedia_edges")
+
+while True:
+    #  Wait for next request from client
+    uri = socket.recv().strip()
+    socket.send(uri_map(uri))
