dbc097c9a92c4ddab02c63b49f9a093a596b4765
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-09-17 11:16:36 +0200

    app generalization

--- kilogram/entity_types/app.py
+++ kilogram/entity_types/app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
+"""
+./app.py typogram
+"""
+import sys
 from flask import Flask, jsonify, request
 from kilogram.dataset.dbpedia import DBPediaOntology
 from kilogram.entity_types.prediction import NgramTypePredictor
 from kilogram import NgramService
 from kilogram.lang.unicode import strip_unicode
 
-NgramService.configure(hbase_host=('diufpc304', 9090))
+NgramService.configure(hbase_host=('diufpc304', 9090), subst_table=sys.argv[1])
 
 app = Flask(__name__)
 dbpedia_ontology = DBPediaOntology('dbpedia_2015-04.owl')
-ngram_predictor = NgramTypePredictor("ngram_types", dbpedia_ontology)
+ngram_predictor = NgramTypePredictor(sys.argv[1], dbpedia_ontology)
 
 
 @app.route('/predict/types/context', methods=['GET'])
