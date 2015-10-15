74117996d22cbf4e799d1df0f8d328f785f16912
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-10-15 14:14:35 -0400

    fix

--- mapreduce/dbpedia_dbm.py
+++ mapreduce/dbpedia_dbm.py
@@ -31,7 +31,7 @@ for line in p.stdout:
     except:
         continue
     if '<http://schema.org/Person>' in type_uri:
-        type_uri = '<http://dbpedia.org/ontology/Person>'
+        type_uri = '<http://dbpedia.org/ontology/Person>   '
     if 'http://dbpedia.org/ontology/' not in type_uri:
         continue
     if 'http://dbpedia.org/ontology/Wikidata' in type_uri:
