83f402d406e557a429a8de2f1a40d0a8fda4aee0
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-10-29 14:31:33 +0100

    new max typed uri

--- kilogram/entity_linking/priorprob/__init__.py
+++ kilogram/entity_linking/priorprob/__init__.py
@@ -4,6 +4,23 @@ from kilogram import NgramService
 
 def get_max_uri(text, table="wiki_anchor_ngrams"):
     try:
-        return max(parse_candidate(NgramService.hbase_raw(table, text, "ngram:value")), key=lambda x: x[1])[0]
+        candidates = parse_candidate(NgramService.hbase_raw(table, text, "ngram:value"))
+        if not candidates:
+            candidates = parse_candidate(NgramService.hbase_raw(table, text.title(), "ngram:value"))
+        return max(candidates, key=lambda x: x[1])[0]
     except:
         return None
+
+
+def get_max_typed_uri(text, e_type, ner, table="wiki_anchor_ngrams"):
+    for mention in (text, text.title()):
+        candidates = parse_candidate(NgramService.hbase_raw(table, mention, "ngram:value"))
+        for cand_uri, _ in sorted(candidates, key=lambda x: x[1], reverse=True):
+            try:
+                cand_type = ner.get_type(cand_uri, -1)
+                if cand_type == e_type:
+                    return cand_uri
+            except IndexError:
+                return cand_uri
+    return None
+
