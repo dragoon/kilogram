892169a1d890216e0781c42a9a02f7d380cd88df
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-11-23 14:25:38 +0100

    migrate to entities list

--- kilogram/entity_linking/rel_rw/__init__.py
+++ kilogram/entity_linking/rel_rw/__init__.py
@@ -2,26 +2,27 @@ from __future__ import division
 import math
 import numpy as np
 import networkx as nx
+from ..util.ml import Feature
 from kilogram import NgramService
 
 
 def _candidate_filter(candidates):
 
     def string_similar(candidate_, topn=10):
-        substring_similar = [x for x in candidate_.uri_counts.items()
-                             if set(candidate_.cand_string.lower().split()).intersection(x[0].lower().split('_'))]
+        substring_similar = [e for e in candidate_.entities
+                             if set(candidate_.cand_string.lower().split()).intersection(e.uri.lower().split('_'))]
         if len(substring_similar) >= topn:
             return substring_similar
-        substring_similar2 = [x for x in candidate_.uri_counts.items()
-                              if candidate_.cand_string in x[0].replace('_', ' ')]
+        substring_similar2 = [e for e in candidate_.entities
+                              if candidate_.cand_string in e.uri.replace('_', ' ')]
         substring_similar.extend(substring_similar2)
         return substring_similar[:topn]
 
     def top_prior(candidate_, topn=10):
-        return sorted(candidate_.uri_counts.items(), key=lambda x: x[1], reverse=True)[:topn]
+        return sorted(candidate_.entities, key=lambda e: e.count, reverse=True)[:topn]
 
     for candidate in candidates:
-        candidate.uri_counts = dict(top_prior(candidate) + string_similar(candidate))
+        candidate.entities = dict(top_prior(candidate) + string_similar(candidate))
 
 
 ALPHA = 0.15  # restart probability
@@ -43,12 +44,12 @@ class SemanticGraph:
 
         candidate_uris = set()
         for cand in candidates:
-            for uri in cand.uri_counts.keys():
-                candidate_uris.add(uri)
-                neighbors[uri] = NgramService.get_wiki_direct_links(uri)
+            for e in cand.entities:
+                candidate_uris.add(e.uri)
+                neighbors[e.uri] = NgramService.get_wiki_direct_links(e.uri)
                 # delete self
                 try:
-                    del neighbors[uri][uri]
+                    del neighbors[e.uri][e.uri]
                 except KeyError:
                     pass
 
@@ -56,17 +57,17 @@ class SemanticGraph:
             """
             :type cand: CandidateEntity
             """
-            for uri in cand.uri_counts.keys():
-                for neighbor, weight in neighbors[uri].iteritems():
-                    if self.G.has_edge(uri, neighbor):
+            for e in cand.entities:
+                for neighbor, weight in neighbors[e.uri].iteritems():
+                    if self.G.has_edge(e.uri, neighbor):
                         continue
                     try:
-                        self.G.add_edge(uri, neighbor, {'w': int(weight)})
+                        self.G.add_edge(e.uri, neighbor, {'w': int(weight)})
                     # happens because of malformed links
                     except ValueError:
                         pass
                 # always add candidates
-                self.G.add_node(uri)
+                self.G.add_node(e.uri)
 
         # prune 1-degree edges except original candidates
         to_remove = set()
@@ -116,11 +117,8 @@ class SemanticGraph:
         """compute document signature"""
         return self._learn_eigenvector(self._get_doc_teleport_v())
 
-    def compute_signatures(self, candidate):
-        signatures = []
-        for uri in candidate.uri_counts.keys():
-            signatures.append((uri, self._learn_eigenvector(self._get_entity_teleport_v(self.index_map[uri]))))
-        return signatures
+    def compute_signature(self, entity):
+        return self._learn_eigenvector(self._get_entity_teleport_v(self.index_map[entity.uri]))
 
     def _zero_kl_score(self, p, q):
         total = 0
@@ -134,20 +132,19 @@ class SemanticGraph:
     def do_linking(self):
         # link unambiguous first
         for candidate in self.candidates:
-            if len(candidate.uri_counts) == 1:
-                candidate.resolved_true_entity = candidate.uri_counts.items()[0][0]
-        for candidate in sorted(self.candidates, key=lambda x: len(x.uri_counts)):
-            if not candidate.uri_counts or candidate.resolved_true_entity:
+            if len(candidate.entities) == 1:
+                candidate.resolved_true_entity = candidate.entities[0].uri
+        for candidate in sorted(self.candidates, key=lambda x: len(x.entities)):
+            if not candidate.entities or candidate.resolved_true_entity:
                 continue
             doc_sign = self.doc_signature()
-            total_uri_count = sum([x for x in candidate.uri_counts.values()], 1)
-            e_signatures = self.compute_signatures(candidate)
+            total_uri_count = sum([e.count for e in candidate.entities], 1)
             cand_scores = []
-            for uri, e_sign in e_signatures:
+            for e in candidate.entities:
+                e_sign = self.compute_signature(e)
                 # global similarity + local (prior prob)
-                sem_sim = 1/self._zero_kl_score(e_sign, doc_sign)
-                          #+ candidate.uri_counts[uri]/total_uri_count
-                cand_scores.append((uri, sem_sim))
+                sem_sim = 1/self._zero_kl_score(e_sign, doc_sign)\
+                          + e.count/total_uri_count
+                cand_scores.append((e.uri, sem_sim))
             max_uri, score = max(cand_scores, key=lambda x: x[1])
             candidate.resolved_true_entity = max_uri
-
