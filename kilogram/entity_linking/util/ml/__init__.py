3756f4b629ed6a0d1647c663b472203a1b08b365
Roman Prokofyev <roman.prokofyev@gmail.com>
2015-11-25 17:02:38 +0100

    init features

--- kilogram/entity_linking/util/ml/__init__.py
+++ kilogram/entity_linking/util/ml/__init__.py
@@ -3,4 +3,15 @@ class Feature(object):
     prior_prob = 0
     type_prob = 0
     type_match = 0
+    type_exists = 0
     context_sim = 0
+    cand_num = 0
+
+    def __init__(self, candidate, entity, graph_score):
+        self.graph_score = graph_score
+        self.cand_num = len(candidate.entities)
+        self.prior_prob = entity.count/sum([e.count for e in candidate.entities], 1)
+        if entity.types:
+            self.type_exists = 1
+            self.type_match = int(candidate.type == entity.get_generic_type())
+
