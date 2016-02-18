4e9e322c5994718ed30706a5899d3189549d7f75
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-18 16:36:19 +0100

    put file directly

--- mapreduce/type_data_generation.sh
+++ mapreduce/type_data_generation.sh
@@ -3,6 +3,8 @@
 
 # compute organic link counts
 spark-submit --master yarn-client --num-executors 10 --executor-memory 3g ./wikipedia/typograms/spark_organic_label_counts.py "/user/roman/dbpedia_data.txt" "/user/roman/wiki_anchors" "/user/roman/organic_label_counts"
+hdfs dfs -cat /user/roman/organic_label_counts/* > organic_label_counts.txt
+spark-submit --executor-memory 3g --num-executors 10 --master yarn-client ./wikipedia/typograms/spark_predicted_label_counts.py "/user/roman/organic_label_counts" "/data/wikipedia_plaintext" "/user/roman/predicted_label_counts"
 
 
 # generate up to 3-grams
--- mapreduce/wikipedia/typograms/spark_predicted_label_counts.py
+++ mapreduce/wikipedia/typograms/spark_predicted_label_counts.py
@@ -1,5 +1,5 @@
 """
-spark-submit --executor-memory 3g --num-executors 10 --master yarn-client ./wikipedia/typograms/spark_predicted_label_counts.py "/user/roman/organic_label_counts" "/data/wikipedia2015_plaintext" "/user/roman/predicted_label_counts"
+spark-submit --executor-memory 3g --num-executors 10 --master yarn-client --files organic_label_counts.txt ./wikipedia/typograms/spark_predicted_label_counts.py "/data/wikipedia_plaintext" "/user/roman/predicted_label_counts"
 """
 import sys
 from pyspark import SparkContext
@@ -8,19 +8,15 @@ from kilogram.dataset.edit_histories.wikipedia import line_filter
 
 sc = SparkContext(appName="WikipediaPredictedLabelCounts")
 
-organic_label_counts = sc.textFile(sys.argv[1])
-wiki_plain = sc.textFile(sys.argv[2])
+wiki_plain = sc.textFile(sys.argv[1])
 
+organic_label_dict = {}
 
-def map_labels(line):
+for line in open("organic_label_counts.txt"):
     uri, label, counts = line.split('\t')
-    if label.islower():
-        return [(label, uri)]
-    else:
-        return [(label, uri), (label.lower(), uri)]
-
-organic_label_dict = dict(organic_label_counts.flatMap(map_labels).collect())
-organic_label_dict_br = sc.broadcast(organic_label_dict)
+    if not label.islower():
+        organic_label_dict[label.lower()] = uri
+    organic_label_dict[label] = uri
 
 
 def generate_ngrams(line):
@@ -35,12 +31,12 @@ def generate_ngrams(line):
                 if i+1 == j and i == 0:
                     # if first word in sentence -> skip, could be wrong (Apple)
                     continue
-                elif token in organic_label_dict_br.value:
-                    labels.append((token.lower(), organic_label_dict_br.value[token], token, 1))
+                elif token in organic_label_dict:
+                    labels.append((token.lower(), organic_label_dict[token], token, 1))
                     i = j-1
                     break
             i += 1
     return labels
 
 wiki_predicted_labels = wiki_plain.flatMap(generate_ngrams)
-wiki_predicted_labels.saveAsTextFile(sys.argv[3])
+wiki_predicted_labels.saveAsTextFile(sys.argv[2])
