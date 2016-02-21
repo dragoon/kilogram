d8297dadcccd1e2237beb64334517ae468aa2fcc
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-21 11:12:28 +0100

    fix linebreaks

--- mapreduce/wikipedia/typograms/spark_generate_typograms.py
+++ mapreduce/wikipedia/typograms/spark_generate_typograms.py
@@ -15,7 +15,7 @@ lines = sc.textFile(sys.argv[1])
 unambiguous_labels = {}
 
 for line in codecs.open("unambiguous_labels.txt", 'r', 'utf-8'):
-    label, uri = line.split('\t')
+    label, uri = line.strip().split('\t')
     unambiguous_labels[label] = uri
 
 
@@ -32,7 +32,7 @@ def generate_ngrams(line):
                 token = ' '.join(sentence[i:j])
                 if i+1 == j and i == 0:
                     # if first word in sentence -> do not attempt to link, could be wrong (Apple)
-                    tokens_plain.append(token)
+                    tokens_plain.append(token.lower())
                 elif token in unambiguous_labels:
                     uri = unambiguous_labels[token]
                     # get types
@@ -45,7 +45,7 @@ def generate_ngrams(line):
                 result.append((' '.join(ngram), 1))
     return result
 
-ngrams = lines.flatMap(generate_ngrams).reduceByKey(lambda n1, n2: n1 + n2).filter(lambda x: x[1] > 1)
+ngrams = lines.flatMap(generate_ngrams).reduceByKey(lambda n1, n2: n1 + n2)
 
 #ner = NgramEntityResolver("dbpedia_data.txt", "dbpedia_2015-04.owl")
 # free some space
