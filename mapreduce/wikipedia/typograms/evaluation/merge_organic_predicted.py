1c659fb4cd3b38dd37a4fc64d895a1f745b65c04
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-22 16:09:37 +0100

    generate merged file

--- mapreduce/wikipedia/typograms/evaluation/merge_organic_predicted.py
+++ mapreduce/wikipedia/typograms/evaluation/merge_organic_predicted.py
@@ -4,11 +4,22 @@ import sys
 organic_file = open(sys.argv[1]).read().splitlines()
 predicted_file = open(sys.argv[2]).read().splitlines()
 
-organic_data = [x.split('\t') for x in organic_file]
-predicted_data = [x.split('\t') for x in predicted_file]
+organic_data = [tuple(x.split('\t')) for x in organic_file]
+predicted_data = [tuple(x.split('\t')) for x in predicted_file]
 
 
-overlap_len = len(set(organic_data).intersection(predicted_data))
+predicted_data_set = set(predicted_data)
+overlap_len = len(predicted_data_set.intersection(organic_data))
 
 print('Overlap:', overlap_len,
       'Percentage:', overlap_len/min(len(organic_data), len(predicted_data)))
+
+
+for line in organic_data:
+    label, uri, sentence = line
+    if label.lower().replace(' ', '_') in uri.lower() and line not in predicted_data_set:
+        predicted_data.append((label, uri, sentence))
+
+
+for line in predicted_data:
+    print('\t'.join(line))
