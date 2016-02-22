f20a9d9802945ece2880441cf7ca4ba9adc7df5c
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-22 10:53:30 +0100

    evaluation doc

--- mapreduce/wikipedia/typograms/evaluation/select_random_articles.py
+++ mapreduce/wikipedia/typograms/evaluation/select_random_articles.py
@@ -2,12 +2,23 @@ import random
 import string
 import os
 import shutil
+import argparse
 
 
-out = 'EVAL'
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('-n', dest='article_num', action='store', required=False, type=int,
+                    default=20, help='number of articles to select')
+parser.add_argument('output_dir', required=True,
+                    help='path to the directory to output random articles')
+
+args = parser.parse_args()
+
+
+
+out = args.output_dir
 os.makedirs(out)
 # 20 random articles:
-for _ in range(20):
+for _ in range(args.article_num):
     first_letter = random.choice('AB')
     second_letter = random.choice(string.uppercase)
     letter_path = first_letter+second_letter
--- mapreduce/wikipedia/typograms/type_data_generation.sh
+++ mapreduce/wikipedia/typograms/type_data_generation.sh
@@ -7,14 +7,20 @@ hdfs dfs -cat /user/roman/organic_label_counts/* > organic_label_counts.txt
 spark-submit --executor-memory 5g --num-executors 10 --master yarn-client --files organic_label_counts.txt ./wikipedia/typograms/spark_predicted_label_counts.py "/data/wikipedia_plaintext" "/user/roman/predicted_label_counts"
 hdfs dfs -cat /user/roman/predicted_label_counts/* > predicted_label_counts.txt
 
-### Generate unambiguous_labels.txt
+### Generate unambiguous_labels.txt (only for typed entities)
 python ./wikipedia/typograms/generate_unambiguous_labels.py
 
 
 ### Generate up to 3-grams
 spark-submit --num-executors 10 --executor-memory 5g --master yarn-client --files unambiguous_labels.txt ./wikipedia/typograms/spark_generate_linked_ngrams.py "/data/wikipedia_plaintext" "/user/roman/wikipedia_linked_ngrams" 3
-spark-submit --num-executors 10 --executor-memory 5g --master yarn-client --files dbpedia_data.txt,dbpedia_2015-04.owl ./wikipedia/typograms/spark_generate_typograms.py "/user/roman/wikipedia_linked_ngrams" "/user/roman/wikipedia_typed_ngrams" 3
+spark-submit --num-executors 10 --executor-memory 5g --master yarn-client --files dbpedia_data.txt,dbpedia_2015-04.owl ./wikipedia/typograms/spark_generate_typograms.py "/user/roman/wikipedia_linked_ngrams" "/user/roman/wikipedia_typed_ngrams"
 
 
 ### Put into Hbase:
 pig -p table=typogram -p path=/user/roman/hbase_wikipedia_typed_ngrams ../extra/hbase_upload_array.pig
+
+
+# EVALUATION
+bzcat ../../nfs/Wikipedia_Text/enwiki-20150602-pages-articles.xml.bz2 | python WikiExtractor.py > out.txt
+# randomly select 20 articles from Wikipedia, output to EVAL
+python generate_eval.py -n 20 --output_dir EVAL
