b71e8ae4850eafb873288a009a52e14dd96fb8f2
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-23 14:17:15 +0100

    added evaluation script

---
+++ mapreduce/wikipedia/typograms/evaluation/evaluate.py
@@ -0,0 +1,33 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--gold-file', required=True,
+                    help='path to the file with the ground truth')
+parser.add_argument('--eval-file', required=True,
+                    help='path to the file to evaluate')
+parser.add_argument('--out-file', required=True, default="not_ranked.txt",
+                    help='path to the file to output non-ranked items')
+
+args = parser.parse_args()
+
+
+gold_data = {}
+
+for line in open(args.gold_file):
+    correct, uri_equals, full_url, token, uri, sentence = line.strip().split('\t')
+    gold_data[(token, uri, sentence)] = int(correct)
+
+
+not_ranked_file = open(args.out_file)
+
+
+labels = []
+for line in open(args.eva_file):
+    token, uri, sentence = line.strip().split('\t')
+    if (token, uri, sentence) in gold_data:
+        labels.append(gold_data[(token, uri, sentence)])
+    else:
+        not_ranked_file.write('\t'.join([token, uri, sentence])+'\n')
+
+not_ranked_file.close()
