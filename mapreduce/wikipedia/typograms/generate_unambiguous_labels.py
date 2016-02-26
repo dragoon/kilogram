6f80ecceae96ebb53769fa51fc69983234b9170d
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-26 14:44:09 +0100

    added arguments

--- mapreduce/wikipedia/typograms/generate_unambiguous_labels.py
+++ mapreduce/wikipedia/typograms/generate_unambiguous_labels.py
@@ -1,9 +1,16 @@
 # coding=utf-8
 from __future__ import division
+import argparse
 import re
 import pandas as pd
 
 NON_CHAR_RE = re.compile(r'[\W\d]+$')
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--ratio-limit', default=20, type=int,
+                    help='ratio limit for labels')
+
+args = parser.parse_args()
 
 
 count_dict = {}
@@ -33,5 +40,5 @@ for row in counts_df.iterrows():
         print(label+'\t'+uri)
         continue
     # write a normal-case label if ratio is less than 20 (means we do not link something that suddenly become super popular - probably an error)
-    if row['organ_count'] > 1 and row['infer_count']/(row['organ_count']) < 20:
+    if row['organ_count'] > 1 and row['infer_count']/(row['organ_count']) < args.ratio_limit:
         print(label+'\t'+uri)
