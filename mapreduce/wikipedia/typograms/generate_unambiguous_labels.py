8538d5daffc8cc06f56b3724753bf9d158074f34
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-26 14:23:24 +0100

    generate unambig labels improve

--- mapreduce/wikipedia/typograms/generate_unambiguous_labels.py
+++ mapreduce/wikipedia/typograms/generate_unambiguous_labels.py
@@ -1,55 +1,37 @@
 # coding=utf-8
 from __future__ import division
+import re
 import pandas as pd
-from collections import defaultdict
+
+NON_CHAR_RE = re.compile(r'[\W\d]+$')
 
 
 count_dict = {}
 for line in open('predicted_label_counts.txt'):
-    uri, label, values = line.split('\t')
-    upper_count, lower_count = values.split(',')
-    count_dict[(uri, label)] = {'infer_normal': int(upper_count), 'infer_lower': int(lower_count), 'len': len(label.split('_')),
-                       'label': label, 'organ_normal': 0, 'organ_lower': 0, 'uri': uri}
+    label, uri, count = line.split('\t')
+    count_dict[(uri, label)] = {'infer_count': int(count), 'len': len(label.split('_')),
+                       'label': label, 'organ_count': 0, 'uri': uri}
 for line in open('organic_label_counts.txt'):
-    uri, label, values = line.split('\t')
+    label, uri, count = line.split('\t')
     if (uri, label) in count_dict:
-        upper_count, lower_count = values.split(',')
-        count_dict[(uri, label)].update({'organ_normal': int(upper_count), 'organ_lower': int(lower_count)})
+        count_dict[(uri, label)].update({'organ_count': int(count)})
 counts_df = pd.DataFrame(count_dict.values())
 del count_dict
-print counts_df.head()
 
-"""
-We never exclude uppercase labels since we don't match at the beginning of a sentence
-"""
-includes = open('unambiguous_labels.txt', 'w')
+
 for row in counts_df.iterrows():
     row = row[1]
     exclude = False
     label = row['label']
     uri = row['uri']
 
+    if NON_CHAR_RE.match(label):
+        continue
+
     # skip uppercase
     if label.isupper():
-        includes.write(label+'\t'+uri+'\n')
+        print(label+'\t'+uri)
         continue
-    # if label appears only in lowercase - add to lower includes
-    if row['organ_normal'] == 0:  # means label is lowercase
-        if row['organ_lower'] > 1 and row['infer_lower']/(row['organ_lower']) < 20:
-            includes.write(label+'\t'+uri+'\n')
-        continue
-    else:
-        infer_ratio = row['infer_normal']/(row['infer_lower'] or 1)
-        orig_ratio = row['organ_normal']/(row['organ_lower'] or 1)
-        if infer_ratio == 0:
-            # weird label, p. ex. 中华人民共和国
-            continue
-        # write a normal-case label if ratio is less than 20 (means we do not link something that suddenly become super popular - probably an error)
-        if row['infer_normal']/row['organ_normal'] < 20 and row['organ_normal'] > 1:
-            includes.write(label+'\t'+uri+'\n')
-        # infer_lower == 0 simply irrelevant, we could not find it
-        # again check < 20 condition
-        if row['infer_lower'] > 0 and row['organ_lower'] > 1 and row['infer_lower']/row['organ_lower'] < 20:
-        #if orig_ratio/infer_ratio < 2:
-            includes.write(label.lower()+'\t'+uri+'\n')
-includes.close()
+    # write a normal-case label if ratio is less than 20 (means we do not link something that suddenly become super popular - probably an error)
+    if row['organ_count'] > 1 and row['infer_count']/(row['organ_count']) < 20:
+        print(label+'\t'+uri)
