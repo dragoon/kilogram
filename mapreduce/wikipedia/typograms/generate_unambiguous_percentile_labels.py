4739eb4067e8b4f0a573a639849d901e1afd5789
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-26 09:21:11 +0100

    generate based on percentile

---
+++ mapreduce/wikipedia/typograms/generate_unambiguous_percentile_labels.py
@@ -0,0 +1,29 @@
+from __future__ import division
+import argparse
+import sys
+from kilogram import ListPacker
+from kilogram.lang.tokenize import default_tokenize_func, tokenize_possessive
+
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--percentile', default=0.9, type=float,
+                    help='percentile to filter')
+args = parser.parse_args()
+
+
+def filter_labels(line):
+    label, uri_list = line.split('\t')
+    # tokenize for commas
+    label = ' '.join(tokenize_possessive(default_tokenize_func(label)))
+    # should be only one
+    uri_counts = [(uri, int(count)) for uri, count in ListPacker.unpack(uri_list)]
+    total = sum(zip(*uri_counts)[1])
+
+    for uri, count in uri_counts:
+        if count/total > args.percentile:
+            print(label, uri)
+            break
+
+
+for line in sys.stdin:
+    filter_labels(line)
