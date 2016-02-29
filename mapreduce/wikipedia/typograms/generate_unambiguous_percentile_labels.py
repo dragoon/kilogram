ca447706df163db004b3f7f91c0370cd2f754325
Roman Prokofyev <roman.prokofyev@gmail.com>
2016-02-29 09:45:53 +0100

    add count to percentile

--- mapreduce/wikipedia/typograms/generate_unambiguous_percentile_labels.py
+++ mapreduce/wikipedia/typograms/generate_unambiguous_percentile_labels.py
@@ -27,7 +27,7 @@ def filter_labels(line):
     for uri, count in uri_counts:
         if count/total > args.percentile and count > args.min_count:
             if uri in ner.dbpedia_types:
-                print(label + '\t' + uri)
+                print(label + '\t' + uri + '\t' + unicode(count))
             break
 
 
